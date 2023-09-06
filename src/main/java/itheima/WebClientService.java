bf72ac534623e185a65abdfb443a5492d4886a6d
wuling <18262626329@163.com>
2023-09-06 08:35:12 +0800

    push commit1

--- src/main/java/itheima/WebClientService.java
+++ src/main/java/itheima/WebClientService.java
@@ -15,12 +15,32 @@ public class WebClientService {
         driver.findElement(By.id("pc_change_know")).click();
         driver.findElement(By.id("pc_know")).click();
         //driver.close();
-        driver.findElement(By.id("pc_date")).sendKeys("2023-09-19");
+        driver.findElement(By.id("pc_date")).sendKeys("2023-09-20");
         List<WebElement> elements = driver.findElements(By.className("layui-icon-ok"));
         elements.get(3).click();elements.get(4).click();elements.get(5).click();
         driver.findElement(By.id("dyy2")).click();
+
+        driver.findElements(By.className("layui-edge")).get(5).click();
         driver.findElement(By.id("pc_danwei")).sendKeys("安徽省水利水电勘测设计研究总院有限公司");
 
-        System.out.println("test master");
+        List<WebElement> dd = driver.findElements(By.tagName("dd"));
+        dd.get(26).click();
+
+        driver.findElements(By.className("layui-edge")).get(6).click();
+        dd.get(37).click();
+
+        driver.findElement(By.id("pc_small")).sendKeys("25");
+
+        driver.findElements(By.className("layui-edge")).get(7).click();
+        dd.get(46).click();
+
+        driver.findElement(By.id("pc_name")).sendKeys("王静");
+        driver.findElement(By.id("pc_phone")).sendKeys("13805604923");
+        driver.findElement(By.id("pc_email")).sendKeys("1270751771@qq.com");
+
+        driver.findElement(By.className("pc_upload")).click();
+
+        //driver.findElement(By.className("pc_upload")).sendKeys("C:\\Users\\Administrator\\Desktop\\1.png");
+        //driver.findElement(By.className("pc_submit")).click();
     }
 }
